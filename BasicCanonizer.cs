b07af1bfce3d0959b4bc981a1fdab5a6414b4d1f
milomacphail <milomacphail@gmail.com>
2019-10-06 08:57:37 -0400

    Add project files.

---
+++ BasicCanonizer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharpTDDTutorial
+{
+    public class BasicCanonizer
+    {
+	    public static string getCanonicalSearch(string searchTerm)
+	    {
+            if (searchTerm == null)
+            {
+                throw new ArgumentNullException("searchTerm");
+            }
+
+            return searchTerm
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.ToUpper())
+                .OrderBy(x => x)
+                .Aggregate(" ", (x, y) => x + " " + y)
+                .Trim();
+	    }
+    } 
+
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharpTDDTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string empty = BasicCanonizer.getCanonicalSearch(" ");
+            Console.WriteLine(empty== "");
+            empty = BasicCanonizer.getCanonicalSearch(" ");
+            Console.WriteLine(empty == "");
+            empty = BasicCanonizer.getCanonicalSearch("       ");
+            Console.WriteLine(empty == "");
+            Console.WriteLine(BasicCanonizer.getCanonicalSearch(" Arsenal Football Club  "));
+            Console.WriteLine(BasicCanonizer.getCanonicalSearch(" Arsenal      Football        Club       "));
+            Console.WriteLine(BasicCanonizer.getCanonicalSearch("Arsenal FC"));
+            Console.WriteLine(BasicCanonizer.getCanonicalSearch("Arsenal Football Club"));
+            Console.WriteLine(BasicCanonizer.getCanonicalSearch("Arsenal FootballClub London"));
+
+            Console.Read();
+        }
+    }
+}
