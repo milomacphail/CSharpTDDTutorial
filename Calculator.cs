58ec0f4b10ebf6ca91c4bac1428899ada00bf904
milomacphail <milomacphail@gmail.com>
2019-10-11 15:20:53 -0400

    Red-Green-Refactor

---
+++ Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Business.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AddNumbers_ValidValues_ReturnsCorrectResult()
+        {
+            var sut = new Calculator();
+            int result = sut.AddNumbers(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+
+    
+    public class Calculator
+    {
+        public int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
